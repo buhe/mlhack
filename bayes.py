c85935bbdee0c6e174f412724400eef1e6228c7c
harebu <bugu1986@126.com>
2014-11-14 18:31:03 +0800

    update hayes

--- bayes.py
+++ bayes.py
@@ -1,3 +1,5 @@
+#encoding=utf-8
+from numpy import *
 def loadDataSet():
 	postingList=[['my','dog','has','flea','problems','help','please'],
 				['maybe','not','take','him','to','dog','park','dtupid'],
@@ -8,3 +10,37 @@ def loadDataSet():
 				]
 	classVec = [0,1,0,1,0,1]
 	return postingList,classVec
+
+def createVocabList(dataSet):
+	vocabSet = set([])
+	for document in dataSet:
+		vocabSet = vocabSet | set(document) #两个集合的并集
+	return list(vocabSet)
+
+def setOfWords2Vec(vocabList,inputSet):#输入语句(inputSet)在单词表(vocabList)中的位置
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] = 1
+		else:
+			print "the word: %s is not in my Vocabulary!" % word
+	return returnVec
+
+def trainNB0(trainMatrix,trainCategory):
+	numTrainDocs = len(trainMatrix)
+	numWords = len(trainMatrix[0])
+	pAbusive = sum(trainCategory)/float(numTrainDocs)
+	p0Num = zeros(numWords)
+	p1Num = zeros(numWords)
+	p0Denom = 0.0
+	p1Denom = 0.0
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = p1Num / p1Denom
+	p0Vect = p0Num / p0Denom
+	return p0Vect,p1Vect,pAbusive
