6a84facb0e50d178a80b75a3456e199226ce7e7c
harebu <bugu1986@126.com>
2014-11-17 11:40:28 +0800

    添加 训练函数

--- bayes.py
+++ bayes.py
@@ -26,16 +26,16 @@ def setOfWords2Vec(vocabList,inputSet):#输入语句(inputSet)在单词表(vocab
 			print "the word: %s is not in my Vocabulary!" % word
 	return returnVec
 
-def trainNB0(trainMatrix,trainCategory):
+def trainNB0(trainMatrix,trainCategory):#trainMatrix 被setOfWords2Vec 处理成向量化,既出现的单词在单词表的位置+1
 	numTrainDocs = len(trainMatrix)
 	numWords = len(trainMatrix[0])
-	pAbusive = sum(trainCategory)/float(numTrainDocs)
-	p0Num = zeros(numWords)
+	pAbusive = sum(trainCategory)/float(numTrainDocs) #计算P(1) 的概率,sum(trainCategory)是概率为1的语句和,float(numTrainDocs) 有几个语句
+	p0Num = zeros(numWords)#一维数组,都是0,最终算出,非侮辱性语句中,每个单词出现的次数
 	p1Num = zeros(numWords)
 	p0Denom = 0.0
 	p1Denom = 0.0
-	for i in range(numTrainDocs):
-		if trainCategory[i] == 1:
+	for i in range(numTrainDocs): #遍历6个文档
+		if trainCategory[i] == 1: #如果文档是 侮辱性的
 			p1Num += trainMatrix[i]
 			p1Denom += sum(trainMatrix[i])
 		else:
@@ -44,3 +44,44 @@ def trainNB0(trainMatrix,trainCategory):
 	p1Vect = p1Num / p1Denom
 	p0Vect = p0Num / p0Denom
 	return p0Vect,p1Vect,pAbusive
+
+def trainNB1(trainMatrix,trainCategory):#trainMatrix 被setOfWords2Vec 处理成向量化,既出现的单词在单词表的位置 = 1
+	numTrainDocs = len(trainMatrix)
+	numWords = len(trainMatrix[0])
+	pAbusive = sum(trainCategory)/float(numTrainDocs) #计算P(1) 的概率,sum(trainCategory)是概率为1的语句和,float(numTrainDocs) 有几个语句
+	p0Num = ones(numWords)#一维数组,都是1,最终算出,非侮辱性语句中,每个单词出现的次数
+	p1Num = ones(numWords)
+	p0Denom = 2.0
+	p1Denom = 2.0
+	for i in range(numTrainDocs): #遍历6个文档
+		if trainCategory[i] == 1: #如果文档是 侮辱性的
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = log(p1Num / p1Denom)
+	p0Vect = log(p0Num / p0Denom)
+	return p0Vect,p1Vect,pAbusive
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+	p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+	# 前面是如果单词出现过(向量是1)则统计其概率,最后相加,后面再加上类别出现的概率,就是朴素贝叶斯的概率
+	p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+	if p1 > p0:
+		return 1;
+	else:
+		return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
